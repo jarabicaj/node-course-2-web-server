2d4c8fd788329fda522068b731b46c3fe5467992
jarabicaj <jarabica.j@gmail.com>
2018-07-26 18:10:53 +0200

    adding joboffers html

--- server.js
+++ server.js
@@ -48,6 +48,10 @@ app.get('/about', (req, res) => {
     })
 });
 
+app.get('/joboffers', (req, res) => {
+    res.render('joboffers.hbs')
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to handle request'
